a3ceed051a819827862f60f43407e8ae692bc85d
Tsyblienko <sergey.tsyblienko@bpmobile.com>
2026-08-27 10:48:18 +0300

    added theme setup to UI module

---
+++ ui/src/main/java/sample/idt/tabletgrid/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package sample.idt.tabletgrid.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+internal val LightThemePrimary = Color(0xFF61558C)
+internal val LightThemePrimaryContainer = Color(0xFFE6DDFD)
+internal val LightThemeSecondary = Color(0xFF615B70)
+internal val LightThemeSecondaryContainer = Color(0xFFE6DEF7)
+internal val LightThemeTertiary = Color(0xFF46683E)
+internal val LightThemeError = Color(0xFFA92F24)
+
+internal val DarkThemePrimary = Color(0xFFCCBEFB)
+internal val DarkThemePrimaryContainer = Color(0xFF4A3E72)
+internal val DarkThemeSecondary = Color(0xFFC9C2DB)
+internal val DarkThemeSecondaryContainer = Color(0xFF484457)
+internal val DarkThemeTertiary = Color(0xFFACD39F)
+internal val DarkThemeError = Color(0xFFF3B8AD)
